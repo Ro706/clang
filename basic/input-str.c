78cb669801ab036e6eb67d2ffafb31272e81498f
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-05-22 16:02:31 +0530

    Update input-str.c

--- basic/input-str.c
+++ basic/input-str.c
@@ -4,5 +4,4 @@ void main(){
 	printf("Enter your name:");
 	scanf("%[^\n]s",&name); //reads a string until it encounters a newline character.
 	printf("hello %s",name); //printing the string (%s)
-	return 0;
 }
